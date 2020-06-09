62833ac679c7edf28abcd7f518d9a88bab4e2d4f
erik <ikbenerik@gmail.com>
2020-06-09 08:50:39 +0200

    finished tutorial

--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from projects.models import Project
+
 
 # Create your views here.
-def project_list(request):
-    return render(request, 'projects/index.html')
+def all_projects(request):
+    projects = Project.objects.all()
+    print(projects)
+    return render(request, 'projects/all_projects.html',
+                  {'projects': projects})
+
+
+def project_detail(request, pk):
+    project = Project.objects.get(pk=pk)
+    return render(request, 'projects/project_detail.html',
+                  {'project': project})
